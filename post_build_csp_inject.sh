be5d945e556c6e25c537ed2186abce349fff5d6c
3rdspace1 <3rdspacenz@gmail.com>
2025-01-12 21:12:39 +0000

    Deploy updated build with CSP fixes

--- post_build_csp_inject.sh
+++ post_build_csp_inject.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Define variables
-BUILD_DIR="build/web"
-BUILD_INDEX_FILE="$BUILD_DIR/index.html"
+BUILD_INDEX_FILE="build/web/index.html"
+CSP_META_TAG='<meta http-equiv="Content-Security-Policy" content="default-src '\''self'\''; script-src '\''self'\'' '\''wasm-unsafe-eval'\'' '\''unsafe-eval'\'' https://www.gstatic.com/flutter-canvaskit/; style-src '\''self'\'' '\''unsafe-inline'\''; connect-src '\''self'\''; object-src '\''none'\'';">'
 
-# Define the updated CSP meta tag
-CSP_META_TAG='<meta http-equiv="Content-Security-Policy" content="default-src '\''self'\''; script-src '\''self'\'' '\''wasm-unsafe-eval'\'' https://www.gstatic.com/flutter-canvaskit/; style-src '\''self'\'' '\''unsafe-inline'\''; connect-src '\''self'\''; object-src '\''none'\'';">'
-
-# Check if the build index.html exists
 if [ -f "$BUILD_INDEX_FILE" ]; then
     # Insert the CSP meta tag into the <head> section
     sed -i "/<head>/a $CSP_META_TAG" "$BUILD_INDEX_FILE"
